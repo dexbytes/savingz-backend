576f2bc7d391cb8d5841d83afbfbe43ff24ff0ca
Jitendra Patidar <=>
2022-12-19 14:28:11 +0530

    Bank with card and service

--- routes/web/bank.php
+++ routes/web/bank.php
@@ -1,8 +1,18 @@
 <?php
 
-use App\Http\Livewire\Bank\Create as BankCreate;
-use App\Http\Livewire\Bank\Edit as BankEdit;
-use App\Http\Livewire\Bank\Index as BankIndex;
+use App\Http\Livewire\Bank\Bank\Create as BankCreate;
+use App\Http\Livewire\Bank\Bank\Edit as BankEdit;
+use App\Http\Livewire\Bank\Bank\Index as BankIndex;
+
+
+use App\Http\Livewire\Bank\Cards\Create as CardCreate;
+use App\Http\Livewire\Bank\Cards\Edit as CardEdit;
+use App\Http\Livewire\Bank\Cards\Index as CardIndex;
+
+use App\Http\Livewire\Bank\Services\Category\Create as ServiceCategoryCreate;
+use App\Http\Livewire\Bank\Services\Category\Edit as ServiceCategoryEdit;
+use App\Http\Livewire\Bank\Services\Category\Index as ServiceCategoryIndex;
+
 use Illuminate\Support\Facades\Route;  
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +30,14 @@ Route::group(['middleware' => ['auth']], function () {
 
     Route::get('bank/list', BankIndex::class)->name('bank-management');
     Route::get('bank/edit/{id}', BankEdit::class)->name('edit-bank');
-     Route::get('bank/create', BankCreate::class)->name('add-bank');
+    Route::get('bank/create', BankCreate::class)->name('add-bank');
+   
+    Route::get('card/index', CardIndex::class)->name('cards-management');
+    Route::get('card/edit/{id}', CardEdit::class)->name('edit-card');
+    Route::get('card/create', CardCreate::class)->name('add-card');
+
+    Route::get('service/category/index', ServiceCategoryIndex::class)->name('service-category-management');
+    Route::get('service/category/edit/{id}', ServiceCategoryEdit::class)->name('edit-service-category');
+    Route::get('service/category/create', ServiceCategoryCreate::class)->name('add-service-category');
+
 });
